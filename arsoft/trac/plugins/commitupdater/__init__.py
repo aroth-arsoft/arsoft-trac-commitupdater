fe32232e5cc719fe20589c2b1fbb60cb1e9d47c3
Andreas Roth <aroth@arsoft-online.com>
2013-02-03 10:35:25 +0100

    prepare python module for trac plugins

---
+++ arsoft/trac/plugins/commitupdater/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; mixedindent off; indent-mode python;
+
+__version__ = __import__('pkg_resources').get_distribution('ARSoftTracCommitupdater').version
+from commit_updater import *
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; mixedindent off; indent-mode python;
+
+from setuptools import setup
+
+setup(name='ARSoftTracCommitupdater',
+        version='0.1',
+        description='A plugin to update tickets within Trac when certain keywords are using in commit messages.',
+        author='Andreas Roth',
+        author_email='aroth@arsoft-online.com',
+        url='http://www.arsoft-online.com/',
+        packages=['arsoft.trac.plugins.commitupdater'],
+        keywords = 'trac commit update ticket issue bug',
+        license = 'BSD',
+        install_requires = ['Trac >= 1.0'],
+        entry_points = {
+            'trac.plugins': [
+                'arsoft.trac.plugins.commitupdater = arsoft.trac.plugins.commitupdater',
+                ]
+            }
+        )
