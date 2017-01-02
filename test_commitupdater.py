ec3632144a62fbffa9e38248c183128fd454a82d
Andreas Roth <aroth@arsoft-online.com>
2017-01-02 14:07:51 +0100

    improve test setup and add test_ready test

--- test_commitupdater.py
+++ test_commitupdater.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # kate: space-indent on; indent-width 4; mixedindent off; indent-mode python;
- 
+
 import sys 
 import os.path
 
@@ -10,17 +10,26 @@ import trac.env
 import time, unittest
 from trac.core import ComponentManager
 from trac.versioncontrol.api import Repository, Changeset
+from tracopt.versioncontrol.git.git_fs import GitRepository
 
 class test_commitupdater(unittest.TestCase):
 
     def setUp(self):
         self._script_dir = os.path.abspath(os.path.dirname(__file__))
-        self._env_dir = os.path.join(self._script_dir, 'tmp', 'env')
-
-        self._env = trac.env.open_environment(self._env_dir)
+        self._tmp_dir = os.path.join(self._script_dir, 'tmp')
+        if not os.path.isdir(self._tmp_dir):
+            os.mkdir(self._tmp_dir)
+        self._env_dir = os.path.join(self._tmp_dir, 'env')
+        if not os.path.isdir(self._env_dir):
+            self._env = trac.env.Environment(path=self._env_dir, create=True)
+        else:
+            self._env = trac.env.open_environment(self._env_dir)
         self._committicketupdater = CommitTicketUpdater(self._env)
 
-        self._repo = Repository("Test_repo",{'name': "Test_repo", 'id': 4321},"tmp.log")
+        self._commit_hash = 'd952a7d7d0c24c02feef500ecbe141e49ff84708'
+
+        #self._repo = Repository("Test_repo",{'name': "Test_repo", 'id': 4321},"tmp.log")
+        self._repo = GitRepository(self._env, path=self._script_dir, params={'name': "Test_repo", 'id': 4321}, log=self._env.log)
 
         # Set all component objects to defaults
         self._env.config.set("ticket","commit_ticket_update_commands.close","close closed closes fix fixed fixes")
@@ -31,6 +40,7 @@ class test_commitupdater(unittest.TestCase):
         self._env.config.set("ticket","commit_ticket_update_commands.worksforme","worksforme")
         self._env.config.set("ticket","commit_ticket_update_commands.alreadyimplemented","alreadyimplemented already_implemented")
         self._env.config.set("ticket","commit_ticket_update_commands.reopen","reopen reopens reopened")
+        self._env.config.set("ticket","commit_ticket_update_commands.testready","testready test_ready ready_for_test rft")
         self._env.config.set("ticket","commit_ticket_update_allowed_domains","mydomain")
 
     def build_comment(self,changeset):
@@ -50,7 +60,7 @@ class test_commitupdater(unittest.TestCase):
 
     def test_check_implements(self):
         message = "Fixed some stuff. implements #1"
-        test_changeset = Changeset(None,1234,message,"test_person",time.time())
+        test_changeset = Changeset(None,self._commit_hash,message,"test_person",time.time())
         self.check_ticket_comment(test_changeset)
         # For each object in turn:
         # Get tickets and commands
@@ -62,7 +72,7 @@ class test_commitupdater(unittest.TestCase):
 
     def test_check_invalidate(self):
         message = "Fixed some stuff. invalid #1"
-        test_changeset = Changeset(None,1234,message,"test_person",time.time())
+        test_changeset = Changeset(None,self._commit_hash,message,"test_person",time.time())
         self.check_ticket_comment(test_changeset)
         # For each object in turn:
         # Get tickets and commands
@@ -74,7 +84,7 @@ class test_commitupdater(unittest.TestCase):
 
     def test_check_rejects(self):
         message = "Fixed some stuff. reject #1"
-        test_changeset = Changeset(None,1234,message,"test_person",time.time())
+        test_changeset = Changeset(None,self._commit_hash,message,"test_person",time.time())
         self.check_ticket_comment(test_changeset)
         # For each object in turn:
         # Get tickets and commands
@@ -86,7 +96,7 @@ class test_commitupdater(unittest.TestCase):
 
     def test_check_worksforme(self):
         message = "Fixed some stuff. worksforme #1"
-        test_changeset = Changeset(None,1234,message,"test_person",time.time())
+        test_changeset = Changeset(None,self._commit_hash,message,"test_person",time.time())
         self.check_ticket_comment(test_changeset)
         # For each object in turn:
         # Get tickets and commands
@@ -98,7 +108,7 @@ class test_commitupdater(unittest.TestCase):
 
     def test_check_alreadyimplemented(self):
         message = "Fixed some stuff. alreadyimplemented #1"
-        test_changeset = Changeset(None,1234,message,"test_person",time.time())
+        test_changeset = Changeset(None,self._commit_hash,message,"test_person",time.time())
         self.check_ticket_comment(test_changeset)
         # For each object in turn:
         # Get tickets and commands
@@ -110,7 +120,7 @@ class test_commitupdater(unittest.TestCase):
 
     def test_check_already_implemented(self):
         message = "Fixed some stuff. already_implemented #1"
-        test_changeset = Changeset(None,1234,message,"test_person",time.time())
+        test_changeset = Changeset(None,self._commit_hash,message,"test_person",time.time())
         self.check_ticket_comment(test_changeset)
         # For each object in turn:
         # Get tickets and commands
@@ -122,7 +132,7 @@ class test_commitupdater(unittest.TestCase):
 
     def test_check_reopens(self):
         message = "Fixed some stuff. reopen #1"
-        test_changeset = Changeset(None,1234,message,"test_person",time.time())
+        test_changeset = Changeset(None,self._commit_hash,message,"test_person",time.time())
         self.check_ticket_comment(test_changeset)
         # For each object in turn:
         # Get tickets and commands
@@ -132,16 +142,28 @@ class test_commitupdater(unittest.TestCase):
         # Now check the actions are right
         self.assertEqual(tickets.get(1),[self._committicketupdater.cmd_reopens])
 
+    def test_check_testready(self):
+        message = "Fixed some stuff. ready_for_test #1"
+        test_changeset = Changeset(None,self._commit_hash,message,"test_person",time.time())
+        self.check_ticket_comment(test_changeset)
+        # For each object in turn:
+        # Get tickets and commands
+        tickets = self._committicketupdater._parse_message(message)
+        # First, check we've got the tickets we were expecting
+        self.assertEqual(tickets.keys(),[1])
+        # Now check the actions are right
+        self.assertEqual(tickets.get(1),[self._committicketupdater.cmd_testready])
+
     def test_allowed_domains(self):
         message = "Fixed some stuff. reopen #1"
 
-        test_changeset_declined = Changeset(None,1234,message,"test_person <me@gohome.now>",time.time())
+        test_changeset_declined = Changeset(None,self._commit_hash,message,"test_person <me@gohome.now>",time.time())
         self.assertEqual(self._committicketupdater._is_author_allowed(test_changeset_declined.author),False)
 
-        test_changeset_allowed = Changeset(None,1234,message,"test_person <me@mydomain>",time.time())
+        test_changeset_allowed = Changeset(None,self._commit_hash,message,"test_person <me@mydomain>",time.time())
         self.assertEqual(self._committicketupdater._is_author_allowed(test_changeset_allowed.author),True)
 
-        test_changeset_no_domain = Changeset(None,1234,message,"test_person",time.time())
+        test_changeset_no_domain = Changeset(None,self._commit_hash,message,"test_person",time.time())
         self.assertEqual(self._committicketupdater._is_author_allowed(test_changeset_no_domain.author),True)
 
 if __name__ == '__main__':
